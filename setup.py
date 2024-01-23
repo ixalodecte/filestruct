6950c4fedd2ec32a2b23c20503c0a65e8cf7369d
leo <leo_dechaumet@protonmail.com>
2024-01-23 20:34:46 +0100

    add common files

---
+++ setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+import os
+import io
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+setup(
+  name = 'filestruct',
+  packages = ['filestruct'],
+  version = '0.1.1',
+  license='GPLv3+',
+  description = 'A python package to structure files using visual and style informations',   # Give a short description about your library
+  author = 'léo DECHAUMET',
+  author_email = 'leo_dechaumet_research@pm.me',
+  url = 'https://github.com/ixalodecte/filestruct',
+  download_url = 'https://github.com/ixalodecte/filestruct/archive/refs/tags/v0.1-alpha.tar.gz',    # I explain this later on
+  keywords = ['pdf', 'parser', 'layout-analysis'],
+  install_requires=[
+          'pandas',
+          'numpy',
+          'PyMuPDF'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    "Intended Audience :: Science/Research",
+    'Topic :: Utilities',
+    'Topic :: Text Processing',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Programming Language :: Python :: 3',
+  ],
+)
