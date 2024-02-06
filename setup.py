899163394b8aca72ed1e0bf7f22ee9db275e791e
leo <leo_dechaumet@protonmail.com>
2024-02-06 17:38:26 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,20 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = '\n' + f.read()
+print(long_description)
 
 setup(
   name = 'filestruct',
   packages = ['filestruct'],
-  version = '0.1.1',
+  version = '0.2',
   license='GPLv3+',
-  description = 'A python package to structure files using visual and style informations',   # Give a short description about your library
-  long_description=long_description,
-  long_description_content_type='text/markdown',
+  description = 'A python package to structure files using visual and style informations',
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
   author = 'léo DECHAUMET',
   author_email = 'leo_dechaumet_research@pm.me',
   url = 'https://github.com/ixalodecte/filestruct',
-  download_url = 'https://github.com/ixalodecte/filestruct/archive/refs/tags/v0.1-alpha.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ixalodecte/filestruct/archive/refs/tags/v0.2-alpha.tar.gz',
   keywords = ['pdf', 'parser', 'layout-analysis'],
   install_requires=[
           'pandas',
